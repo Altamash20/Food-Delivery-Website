428e083da0075db850e9d744567c23fbc133449c
Mohammad Altamash <altamash201002@gmail.com>
2025-12-21 14:39:02 +0530

    trying to toggle  hamburger bars to xmark

--- main.js
+++ main.js
@@ -13,9 +13,15 @@ const closeBtn = document.querySelector('.close-btn');
 const cardList = document.querySelector('.card-list');
 const cartList = document.querySelector('.cart-list');
 const cartTotal = document.querySelector('.cart-total');
+const cartValue = document.querySelector('.cart-value');
+const hamburger = document.querySelector('.hamburger');
+const mobileMenu = document.querySelector('.mobile-menu');
+const bars = document.querySelector('.fa-bars');
 
 cartIcon.addEventListener('click', () => cartTab.classList.add('cart-tab-active'));
 closeBtn.addEventListener('click', () => cartTab.classList.remove('cart-tab-active'));
+hamburger.addEventListener('click', () => mobileMenu.classList.toggle('mobile-menu-active'));
+hamburger.addEventListener('click', () => bars.classList.toggle('fa-xmark'));
 
 let productList = [];
 let cartProduct = []; 
@@ -23,15 +29,19 @@ let cartProduct = [];
 const updateTotals = () => {
 
     let totalPrice = 0;
+    let totalQuantity = 0;
 
     document.querySelectorAll('.item').forEach(item => {
 
+        const quantity = parseInt(item.querySelector('.quantity-value').textContent);
         const price = parseFloat(item.querySelector('.item-total').textContent.replace('$', ''));
     
         totalPrice += price;
+        totalQuantity += quantity;
     });
 
     cartTotal.textContent = `$${totalPrice.toFixed(2)}`;
+    cartValue.textContent = totalQuantity;
 
 };
 
